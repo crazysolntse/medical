303e0b63fcd819de6a24b9104271d981ea5925ac
crazysolntse <bubnova.elena@gmail.com>
2025-06-03 19:20:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,16 @@ async function askAI() {
     }
 
     const answer = data.choices[0]?.message?.content || "Не удалось получить ответ.";
+
+    
+    <!-- marked.js CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js "></script>
+    <script>
+    const output = marked.parse(answer);
+    </script>
+
+
+    
     responseDiv.textContent = answer;
   } catch (err) {
     console.error(err);
